178bd03ea46adf06764545bc81591bd68d129d20
nfonseca123 <149430262+nfonseca123@users.noreply.github.com>
2025-03-12 12:45:33 -0500

    Your commit message

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], // Target JavaScript, TypeScript files including .mjs and .cjs
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'no-unused-vars': 'warn',// Custom rule
+    },
+  },
+];
